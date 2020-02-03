040a1a4016be80ee7d45b4ddfc42667e0ad3dd56
Brian Leonardo de Morais <morais.brian360@gmail.com>
2020-02-03 14:12:28 -0300

    Aula 55 finalizada

---
+++ Secao_07/App04_Xamarin/App04_Xamarin/App04_Xamarin/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App04_Xamarin
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new App04_Xamarin.Master.Menu();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
