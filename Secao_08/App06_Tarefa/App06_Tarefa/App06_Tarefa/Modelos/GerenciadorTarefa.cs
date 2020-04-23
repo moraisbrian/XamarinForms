53061bf2f7b4c58fa1b6b30a4d712ce57539a597
Brian Leonardo de Morais <morais.brian360@gmail.com>
2020-04-23 16:12:33 -0300

    Aula 95 finalizada

---
+++ Secao_08/App06_Tarefa/App06_Tarefa/App06_Tarefa/Modelos/GerenciadorTarefa.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App06_Tarefa.Modelos
+{
+    public class GerenciadorTarefa
+    {
+        private List<Tarefa> Lista { get; set; }
+        public void Salvar(Tarefa tarefa)
+        {
+            Lista.Add(tarefa);
+
+            SalvarProperties(Lista);
+        }
+
+        public void Finalizar(Tarefa tarefa, int index)
+        {
+            Lista.RemoveAt(index);
+
+            Lista.Add(tarefa);
+            SalvarProperties(Lista);
+        }
+
+        public void Deletar(Tarefa tarefa)
+        {
+            Lista.Remove(tarefa);
+
+            SalvarProperties(Lista);
+        }
+
+        public List<Tarefa> Listagem()
+        {
+            return ListagemProperties();
+        }
+
+        public void SalvarProperties(List<Tarefa> lista)
+        {
+            if (App.Current.Properties.ContainsKey("Tarefas"))
+            {
+                App.Current.Properties.Remove("Tarefas");
+            }
+
+            App.Current.Properties.Add("Tarefas", lista);
+        }
+
+        public List<Tarefa> ListagemProperties()
+        {
+            if (App.Current.Properties.ContainsKey("Tarefas"))
+            {
+                return (List<Tarefa>)App.Current.Properties["Tarefas"];
+            }
+            else
+            {
+                return new List<Tarefa>();
+            }
+        }
+    }
+}
