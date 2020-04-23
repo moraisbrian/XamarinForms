6231321a21f081696d33674b0040a3db110debda
Brian Leonardo de Morais <morais.brian360@gmail.com>
2020-04-23 13:47:04 -0300

    Aula 91 finalizada

---
+++ Secao_08/App06_Tarefa/App06_Tarefa/App06_Tarefa/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using App06_Tarefa.Telas;
+
+namespace App06_Tarefa
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new NavigationPage(new Inicio());
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
