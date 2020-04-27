74aef61b11e972e6f73f9be618e2e92ba7a7f213
Brian Leonardo de Morais <morais.brian360@gmail.com>
2020-04-27 14:30:06 -0300

    Aula 128 finalizada

---
+++ Secao_10/App08_Estilo/App08_Estilo/App08_Estilo/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App08_Estilo
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new NavigationPage(new Menu.Master());
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ Secao_10/App08_Estilo/App08_Estilo/App08_Estilo/Menu/Master.xaml.cs
@@ -0,0 +1,45 @@
+﻿using App08_Estilo.Pagina;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App08_Estilo.Menu
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Master : MasterDetailPage
+    {
+        public Master()
+        {
+            InitializeComponent();
+        }
+
+        private void GoImplicitiStylePage(object sender, EventArgs args)
+        {
+            Detail = new NavigationPage(new ImplicitiStylePage());
+            IsPresented = false;
+        }
+
+        private void GoExplicitiStylePage(object sender, EventArgs args)
+        {
+            Detail = new NavigationPage(new ExplicitiStylePage());
+            IsPresented = false;
+        }
+
+        private void GoGlobalStylePage(object sender, EventArgs args)
+        {
+            Detail = new NavigationPage(new GlobalStylePage());
+            IsPresented = false;
+        }
+
+        private void GoInheritStylePage(object sender, EventArgs args)
+        {
+            Detail = new NavigationPage(new InheritStylePage());
+            IsPresented = false;
+        }
+    }
+}
