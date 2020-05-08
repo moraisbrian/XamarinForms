0d74a1e3895daf7073a43438df9c31864ba64a9f
Brian Leonardo de Morais <morais.brian360@gmail.com>
2020-05-08 15:50:25 -0300

    Aula 19 Curso B

--- Curso_B/App02/App02/App02/MainPage.xaml.cs
+++ Curso_B/App02/App02/App02/MainPage.xaml.cs
@@ -24,20 +24,34 @@ namespace App02
         {
             return new List<Grupo>()
             {
-                new Grupo("Presidente", "CEO")
+                new Grupo("Presidente", "CEO", "Presidente da empresa")
                 {
-                    new Pessoa() { Nome = "José" }
+                    new Pessoa() { Nome = "José", Obrigatorio = true, Eficiencia = 8 }
                 },
-                new Grupo("Diretores", "Dir.")
+                new Grupo("Diretores", "Dir.", "Diretor da empresa")
                 {
-                    new Pessoa() { Nome = "Marcos" },
-                    new Pessoa() { Nome = "Maria" }
+                    new Pessoa() { Nome = "Marcos", Obrigatorio = true, Eficiencia = 8 },
+                    new Pessoa() { Nome = "Maria", Obrigatorio = false }
                 },
-                new Grupo("Gerentes", "Ger.")
+                new Grupo("Gerentes", "Ger.", "Gerente da empresa")
                 {
-                    new Pessoa() { Nome = "Antonio" },
-                    new Pessoa() { Nome = "Ana" },
-                    new Pessoa() { Nome = "Anderson" }
+                    new Pessoa() { Nome = "Antonio", Obrigatorio = false },
+                    new Pessoa() { Nome = "Ana", Obrigatorio = true, Eficiencia = 9 }
+                },
+                new Grupo("Funcionarios", "Func.", "Funcionario da empresa")
+                {
+                    new Pessoa() { Nome = "Antonio", Obrigatorio = false },
+                    new Pessoa() { Nome = "Ana", Obrigatorio = false },
+                    new Pessoa() { Nome = "Anderson", Obrigatorio = false },
+                    new Pessoa() { Nome = "Marcos", Obrigatorio = false },
+                    new Pessoa() { Nome = "Maria", Obrigatorio = true, Eficiencia = 6 },
+                    new Pessoa() { Nome = "José", Obrigatorio = false },
+                    new Pessoa() { Nome = "Debora", Obrigatorio = false },
+                    new Pessoa() { Nome = "Marcio", Obrigatorio = false },
+                    new Pessoa() { Nome = "Jessica", Obrigatorio = false },
+                    new Pessoa() { Nome = "Roseli", Obrigatorio = false },
+                    new Pessoa() { Nome = "Fernando", Obrigatorio = false },
+                    new Pessoa() { Nome = "Viviane", Obrigatorio = false }
                 }
             };
         }
@@ -46,17 +60,21 @@ namespace App02
         {
             public string Titulo { get; set; }
             public string TituloCurto { get; set; }
+            public string Descricao { get; set; }
 
-            public Grupo(string titulo, string tituloCurto)
+            public Grupo(string titulo, string tituloCurto, string descricao)
             {
                 this.Titulo = titulo;
                 this.TituloCurto = tituloCurto;
+                this.Descricao = descricao;
             }
         }
 
         public class Pessoa
         {
             public string Nome { get; set; }
+            public int Eficiencia { get; set; }
+            public bool Obrigatorio { get; set; }
         }
     }
 }
