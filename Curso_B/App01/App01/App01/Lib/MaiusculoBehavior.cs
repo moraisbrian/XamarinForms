45f831c8025cced9fde0c02018d90401e48a37ee
Brian Leonardo de Morais <morais.brian360@gmail.com>
2020-05-08 11:54:11 -0300

    Aula 8 Curso B

---
+++ Curso_B/App01/App01/App01/Lib/MaiusculoBehavior.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace App01.Lib
+{
+    public class MaiusculoBehavior : Behavior<Entry>
+    {
+        protected override void OnAttachedTo(Entry bindable)
+        {
+            base.OnAttachedTo(bindable);
+            bindable.TextChanged += MaiusculoAction;
+        }
+
+        protected override void OnDetachingFrom(Entry bindable)
+        {
+            base.OnDetachingFrom(bindable);
+            bindable.TextChanged -= MaiusculoAction;
+        }
+
+        private void MaiusculoAction(object sender, TextChangedEventArgs args)
+        {
+            if (!string.IsNullOrEmpty(args.NewTextValue))
+                ((Entry)sender).Text = args.NewTextValue.ToUpper();
+        }
+    }
+}
