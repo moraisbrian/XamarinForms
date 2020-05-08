46d4ead7c818fc3a6f56860513af59f6262b6496
Brian Leonardo de Morais <morais.brian360@gmail.com>
2020-05-08 14:02:33 -0300

    Aula 14 Curso B

---
+++ Curso_B/App01/App01/App01/Lib/EmailValidoTrigger.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace App01.Lib
+{
+    public class EmailValidoTrigger : TriggerAction<Entry>
+    {
+        protected override void Invoke(Entry sender)
+        {
+            if (sender.Text.IndexOf("@") >= 0)
+            {
+                sender.TextColor = Color.Green;
+            }
+            else
+            {
+                sender.TextColor = Color.Red;
+            }
+        }
+    }
+}
---
+++ Curso_B/App02/App02/App02/MainPage.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App02
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            ListaFuncionarios.ItemsSource = GetFuncionarios();
+        }
+
+        private List<Grupo> GetFuncionarios()
+        {
+            return new List<Grupo>()
+            {
+                new Grupo("Presidente", "CEO")
+                {
+                    new Pessoa() { Nome = "José" }
+                },
+                new Grupo("Diretores", "Dir.")
+                {
+                    new Pessoa() { Nome = "Marcos" },
+                    new Pessoa() { Nome = "Maria" }
+                },
+                new Grupo("Gerentes", "Ger.")
+                {
+                    new Pessoa() { Nome = "Antonio" },
+                    new Pessoa() { Nome = "Ana" },
+                    new Pessoa() { Nome = "Anderson" }
+                }
+            };
+        }
+
+        public class Grupo : List<Pessoa>
+        {
+            public string Titulo { get; set; }
+            public string TituloCurto { get; set; }
+
+            public Grupo(string titulo, string tituloCurto)
+            {
+                this.Titulo = titulo;
+                this.TituloCurto = tituloCurto;
+            }
+        }
+
+        public class Pessoa
+        {
+            public string Nome { get; set; }
+        }
+    }
+}
