855894a4d90125b844a00da34c1b99ed2575f2ad
Brian Leonardo de Morais <morais.brian360@gmail.com>
2020-05-11 15:49:23 -0300

    Aula 33 Curso B

---
+++ Curso_B/App05/App05/App05/Controls/CustomBoxView.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace App05.Controls
+{
+    public class CustomBoxView : BoxView
+    {
+        public static readonly BindableProperty EspessuraProperty = BindableProperty.Create<CustomBoxView, double>(
+                p => p.Espessura, default(double)
+            );
+
+        public double Espessura
+        {
+            get { return (double)GetValue(EspessuraProperty); }
+            set { SetValue(EspessuraProperty, value); }
+        }
+    }
+}
