feef22735b376153a051cb72f7a456aa142c127a
Brian Leonardo de Morais <morais.brian360@gmail.com>
2020-05-13 10:15:48 -0300

    Alteração em pastas do Curso A

---
+++ Curso_A/App14_JWTAsync/App14_JWTAsync/App14_JWTAsync/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using App14_JWTAsync.Service;
+using App14_JWTAsync.Model;
+
+namespace App14_JWTAsync
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+        public async void GetTokenAction(object sender, EventArgs args)
+        {
+            string resultado = await JWTService.GetToken(nome.Text, password.Text);
+            LblToken.Text = resultado;
+
+        }
+        public async void VerificarAction(object sender, EventArgs args)
+        {
+            string resultado = await JWTService.Verificar();
+            LblResultado.Text = resultado;
+        }
+
+    }
+}
