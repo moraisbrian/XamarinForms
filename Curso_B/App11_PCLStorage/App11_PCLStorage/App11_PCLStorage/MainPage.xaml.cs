32d512cecd9ec422b634682192e187db97a025dd
Brian Leonardo de Morais <morais.brian360@gmail.com>
2020-05-13 17:09:33 -0300

    Aula 60 Curso B

---
+++ Curso_B/App11_PCLStorage/App11_PCLStorage/App11_PCLStorage/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using PCLStorage;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using App11_PCLStorage.Util;
+
+namespace App11_PCLStorage
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            btnEscrever.Clicked += (sender, args) =>
+            {
+                StorageManager.SalvarArquivo("file.txt", txtConteudo.Text);
+            };
+
+            btnLerArquivo.Clicked += async (sender, args) =>
+            {
+                lblConteudoArquivo.Text = await StorageManager.LerArquivo("file.txt");
+            };
+        }
+    }
+}
---
+++ Curso_B/App11_PCLStorage/App11_PCLStorage/App11_PCLStorage/Util/StorageManager.cs
@@ -0,0 +1,28 @@
+﻿using PCLStorage;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App11_PCLStorage.Util
+{
+    public class StorageManager
+    {
+        public async static void SalvarArquivo(string arquivo, string conteudo)
+        {
+            IFolder rootFolder = FileSystem.Current.LocalStorage;
+            IFolder folder = await rootFolder.CreateFolderAsync("Arquivos", CreationCollisionOption.OpenIfExists);
+            IFile file = await folder.CreateFileAsync(arquivo, CreationCollisionOption.ReplaceExisting);
+            await file.WriteAllTextAsync(conteudo);
+        }
+
+        public async static Task<string> LerArquivo(string nomeArquivo)
+        {
+            IFolder rootFolder = FileSystem.Current.LocalStorage;
+            IFolder folder = await rootFolder.CreateFolderAsync("Arquivos", CreationCollisionOption.OpenIfExists);
+            IFile file = await folder.GetFileAsync(nomeArquivo);
+
+            return await file.ReadAllTextAsync();
+        }
+    }
+}
