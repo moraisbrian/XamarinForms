34af15d07e6f0536e5f72fe2581b94d65e6891f0
Brian Leonardo de Morais <morais.brian360@gmail.com>
2020-05-14 10:57:35 -0300

    Aula 68 Curso B

--- Curso_B/App12_Realm/App12_Realm/App12_Realm/MainPage.xaml.cs
+++ Curso_B/App12_Realm/App12_Realm/App12_Realm/MainPage.xaml.cs
@@ -1,10 +1,13 @@
-﻿using System;
+﻿using App12_Realm.Modelo;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
+using System.ComponentModel.DataAnnotations;
+using Realms;
 
 namespace App12_Realm
 {
@@ -16,6 +19,103 @@ namespace App12_Realm
         public MainPage()
         {
             InitializeComponent();
+
+            btnAdicionar.Clicked += delegate
+            {
+                Produto produto = new Produto();
+                produto.Item = txtItem.Text;
+                produto.Quantidade = int.Parse(txtQuantidade.Text);
+
+                var listRes = new List<ValidationResult>();
+                var contexto = new ValidationContext(produto);
+                bool valido = Validator.TryValidateObject(produto, contexto, listRes, true);
+
+                if (!valido)
+                {
+                    string mensagem = string.Empty;
+
+                    foreach (var res in listRes)
+                    {
+                        mensagem += res.ErrorMessage + "\n";
+                    }
+                    DisplayAlert("Erros", mensagem, "Ok");
+                }
+                else
+                {
+                    var realm = Realm.GetInstance();
+
+                    if (lblId.Text == string.Empty)
+                    {
+                        #region Adicionar
+                        var produtoFinal = realm.All<Produto>()
+                            .OrderByDescending(x => x.Id)
+                            .FirstOrDefault();
+
+                        int novoId = 1;
+                        if (produtoFinal != null)
+                        {
+                            novoId = produtoFinal.Id + 1;
+                        }
+
+                        produto.Id = novoId;
+
+                        realm.Write(() =>
+                        {
+                            realm.Add(produto);
+                        });
+                        #endregion
+                    }
+                    else
+                    {
+                        #region Atualizar
+                        produto.Id = int.Parse(lblId.Text);
+                        realm.Write(() =>
+                        {
+                            realm.Add(produto, true);
+                        });
+                        #endregion
+                    }
+
+                    DisplayAlert("Salvo com sucesso",
+                        string.Format("Itens no banco de dados: {0}", realm.All<Produto>().Count()),
+                        "Ok");
+
+                    txtItem.Text = "";
+                    txtQuantidade.Text = "";
+                    lblId.Text = "";
+
+                    listaProdutos.ItemsSource = realm.All<Produto>();
+                }
+            };
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            var realm = Realm.GetInstance();
+            listaProdutos.ItemsSource = realm.All<Produto>();
+        }
+
+        private void MenuItemExcluir_Clicked(object sender, EventArgs e)
+        {
+            Produto produto = ((Produto)((MenuItem)sender).CommandParameter);
+            var realm = Realm.GetInstance();
+
+            realm.Write(() =>
+            {
+                realm.Remove(produto);
+            });
+            
+            listaProdutos.ItemsSource = realm.All<Produto>();
+        }
+
+        private void MenuItemEditar_Clicked(object sender, EventArgs args)
+        {
+            Produto produto = ((Produto)((MenuItem)sender).CommandParameter);
+            lblId.Text = produto.Id.ToString();
+            txtItem.Text = produto.Item;
+            txtQuantidade.Text = produto.Quantidade.ToString();
         }
     }
 }
---
+++ Curso_B/App12_Realm/App12_Realm/App12_Realm/Modelo/Produto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel.DataAnnotations;
+using Realms;
+
+namespace App12_Realm.Modelo
+{
+    public class Produto : RealmObject
+    {
+        [PrimaryKey]
+        public int Id { get; set; }
+
+        [System.ComponentModel.DataAnnotations.Required]
+        public string Item { get; set; }
+
+        [System.ComponentModel.DataAnnotations.Required]
+        [Range(1, 999)]
+        public int? Quantidade { get; set; }
+    }
+}
